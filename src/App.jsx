d54a9d591d93063c3debd081ffa40c5256356213
Eimantas Andrejevas <eimutis.a@gmail.com>
2021-07-08 12:20:31 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>blablabla</p>
+        <p className="bg-blue-800">blablabla</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           test
         </a>

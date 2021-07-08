6da07d70dd6c219d0a8f8b4aa5c865d798afa4e6
Eimantas Andrejevas <eimutis.a@gmail.com>
2021-07-08 12:17:46 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>blablabla</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          lololol
+          test
         </a>
       </header>
     </div>

796a03a8955874890a49bb4edb45527773aab959
Eimantas Andrejevas <eimutis.a@gmail.com>
2021-07-08 12:40:35 +0300

    path fix

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,11 @@ function App() {
         </header>
         <main className="p-3" style={{ height: "calc(100vh - 44px)" }}>
           <Switch>
-            <Route exact path={["/eshop-react-app//", "products"]} component={Products} />
-            <Route exact path="/eshop-react-app//products/:id" component={Product} />
-            <Route exact path="/eshop-react-app//vision" component={Vision} />
-            <Route exact patch="/eshop-react-app//cart" component={Cart} />
-            <Route exact path="/eshop-react-app//error" component={Error} />
+            <Route exact path={["/eshop-react-app/", "products"]} component={Products} />
+            <Route exact path="/eshop-react-app/products/:id" component={Product} />
+            <Route exact path="/eshop-react-app/vision" component={Vision} />
+            <Route exact patch="/eshop-react-app/cart" component={Cart} />
+            <Route exact path="/eshop-react-app/error" component={Error} />
           </Switch>
         </main>
       </CartProvider>
